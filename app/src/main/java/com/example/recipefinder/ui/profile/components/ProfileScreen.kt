c3996a52554c34298da864022b281ba268beab3c
NiazSagor <niazsagor@gmail.com>
2025-01-30 22:34:07 +0600

    feat: add saved recipe and activity tabs, implement saved recipe and activity screens
    This commit introduces the following changes:
    
    -   Added a TabRow to the profile screen to switch between "Saved Recipe" and "Activity" views.
    -   Implemented `SavedRecipeScreen` to display bookmarked recipes using `LazyVerticalStaggeredGrid`.
    -   Implemented `ActivityScreen` to display user activity (ratings and tips) using `LazyColumn` and `HorizontalList`.
    -   Added placeholder UI for empty ratings and tips.
    - Added Tabs functionality.
    - Created Saved Recipe and Activity Screen.
    - Removed old saved recipe list.
    - Added rating and tips view in activity tab.

--- app/src/main/java/com/example/recipefinder/ui/profile/components/ProfileScreen.kt
+++ app/src/main/java/com/example/recipefinder/ui/profile/components/ProfileScreen.kt
@@ -10,13 +10,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,9 +44,11 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.example.recipefinder.R
-import com.example.recipefinder.ui.myiconpack.MyIconPack
+import com.example.recipefinder.data.model.Recipe
+import com.example.recipefinder.ui.home.HorizontalList
 import com.example.recipefinder.ui.home.components.BottomNavigationBar
 import com.example.recipefinder.ui.home.elements.RecipeHorizontalListItem
+import com.example.recipefinder.ui.myiconpack.MyIconPack
 import com.example.recipefinder.ui.myiconpack.Wave
 
 // TODO: fix the bottom nav
@@ -45,6 +59,8 @@ fun ProfileScreen(
     onBottomBarClick: (String) -> Unit,
 ) {
     val bookmarkedRecipes = viewmodel.profileState.collectAsStateWithLifecycle()
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val tabs = listOf("Saved Recipe", "Activity")
 
     Scaffold(
         bottomBar = { BottomNavigationBar(onBottomBarClick) }
@@ -69,7 +85,9 @@ fun ProfileScreen(
             )
 
             Column(
-                modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
 
@@ -98,13 +116,19 @@ fun ProfileScreen(
                     )
                 }
 
-
-                Text(
-                    text = "Saved Recipes",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 14.sp,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
+                TabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    backgroundColor = Color.White,
+                    contentColor = MaterialTheme.colorScheme.primary
+                ) {
+                    tabs.forEachIndexed { index, title ->
+                        Tab(
+                            text = { Text(title, color = MaterialTheme.colorScheme.primary) },
+                            selected = selectedTabIndex == index,
+                            onClick = { selectedTabIndex = index }
+                        )
+                    }
+                }
 
                 when (bookmarkedRecipes.value) {
                     is ProfileState.Error -> {}
@@ -113,27 +137,127 @@ fun ProfileScreen(
                     is ProfileState.Success -> {
                         val recipes =
                             (bookmarkedRecipes.value as ProfileState.Success).bookmarkedRecipes
-                        LazyVerticalStaggeredGrid(
-                            columns = StaggeredGridCells.Fixed(2),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            content = {
-                                itemsIndexed(recipes) { index, recipe ->
-                                    RecipeHorizontalListItem(
-                                        getLikesForRecipe = { viewmodel.getRecipeLike(it) },
-                                        recipe = recipe,
-                                        searchItem = true,
-                                        onRecipeClick = { onRecipeClick(it) },
-                                        onSave = { viewmodel.saveRecipe(it) }
-                                    )
+
+                        when (selectedTabIndex) {
+                            0 -> {
+                                SavedRecipeScreen(
+                                    recipes, viewmodel
+                                ) {
+                                    onRecipeClick(it)
                                 }
-                            },
-                        )
+                            }
+
+                            1 -> {
+                                ActivityScreen(
+                                    myRatings = emptyList(),
+                                    myTips = emptyList(),
+                                    getLikesForRecipe = { viewmodel.getRecipeLike(it) },
+                                    onRecipeClick = { onRecipeClick(it) },
+                                    onSave = { viewmodel.saveRecipe(it) }
+                                )
+                            }
+                        }
+
                     }
                 }
             }
         }
     }
+}
 
+@Composable
+fun SavedRecipeScreen(
+    recipes: List<Recipe>,
+    viewmodel: ProfileScreenViewModel,
+    onRecipeClick: (Int) -> Unit,
+) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        content = {
+            itemsIndexed(recipes) { index, recipe ->
+                RecipeHorizontalListItem(
+                    getLikesForRecipe = { viewmodel.getRecipeLike(it) },
+                    recipe = recipe,
+                    searchItem = true,
+                    onRecipeClick = { onRecipeClick(it) },
+                    onSave = { viewmodel.saveRecipe(it) }
+                )
+            }
+        },
+    )
+}
+
+@Composable
+fun ActivityScreen(
+    myRatings: List<Recipe>,
+    myTips: List<Recipe>,
+    getLikesForRecipe: suspend (Int) -> Int,
+    onRecipeClick: (Int) -> Unit,
+    onSave: (Recipe) -> Unit,
+) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        item {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                HorizontalList(
+                    getLikesForRecipe = { getLikesForRecipe(it) },
+                    onRecipeClick = { },
+                    onSave = { },
+                    title = "My Ratings (${myRatings.size})",
+                    recipes = myRatings
+                )
+                if (myRatings.isEmpty()) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth().size(100.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Icon(
+                            tint = MaterialTheme.colorScheme.primary,
+                            imageVector = Icons.Default.ThumbUp,
+                            contentDescription = null
+                        )
+
+                        Text(
+                            fontSize = 12.sp,
+                            text = "Rate your first recipe to see it here."
+                        )
+                    }
+                }
+            }
+        }
+        item {
+            HorizontalList(
+                getLikesForRecipe = { getLikesForRecipe(it) },
+                onRecipeClick = { },
+                onSave = { },
+                title = "My Tips (${myTips.size})",
+                recipes = myTips
+            )
+            if (myTips.isEmpty()) {
+                Column(
+                    modifier = Modifier.fillMaxWidth().size(100.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Icon(
+                        tint = MaterialTheme.colorScheme.primary,
+                        imageVector = Icons.Default.Book,
+                        contentDescription = null
+                    )
+
+                    Text(
+                        fontSize = 12.sp,
+                        text = "Leave your first tip to see it here."
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview
